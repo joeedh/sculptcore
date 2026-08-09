37fde46fda4d75756bcf78257950fb79c6cd39f8
Joe Eagar <joeedh@gmail.com>
2026-08-09 16:10:32 -0700

    fix rundebugapp.sh

--- rundebugapp.sh
+++ rundebugapp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./build/native-msvc/source/debug/debug_app.exe --script test_draw.txt --interactive
+./build/native/source/debug/debug_app.exe --script test_draw.txt --interactive
 
